af2ca5fbd8133d5fdd9bf545cf16c242e0f96861
reyesgar <66829791+reyesgar@users.noreply.github.com>
2024-10-01 10:48:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-}
-resource "azurerm_resource_group" "rg" {
-  location = "East US"
-  name     = "mytestsrg"
 }
